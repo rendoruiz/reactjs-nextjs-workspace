67777dd67ef481b4836434bc078767a8587f6c70
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 14:07:26 -0600

    #4 links

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
+      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
       <Footer />
     </div>
   )
