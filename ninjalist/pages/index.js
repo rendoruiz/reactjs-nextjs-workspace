a80cdfb920f500eb6d77a763e08322482d97f6a1
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 14:11:02 -0600

    #5 layout component

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -8,12 +8,10 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
       <Link href="/ninjas"><a>See Ninja Listing</a></Link>
-      <Footer />
     </div>
   )
 }
