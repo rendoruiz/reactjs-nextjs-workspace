dd8bed6442fb191e6968e5ca38d15a7bb27147ae
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 14:37:42 -0600

    #8 auto redirect

--- ninjalist/pages/404.js
+++ ninjalist/pages/404.js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, []);
+
   return ( 
     <div className="not-found">
       <h1>Ooooops...</h1>
