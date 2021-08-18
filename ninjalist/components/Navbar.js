f5ce742fa32b4b09d38e04ab97d2d49c1ea2a72b
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 14:02:28 -0600

    #3 components

---
+++ ninjalist/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav>
+      <div class="logo">
+        <h1>Ninja List</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Ninja Listing</a>
+    </nav>
+  );
+}
+ 
+export default Navbar;
--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
+      <Footer />
     </div>
   )
 }
