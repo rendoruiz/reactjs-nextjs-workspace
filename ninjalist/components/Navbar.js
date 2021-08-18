56dbf4c6493ce1d04a278b74ac9b37fb88326468
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 15:43:57 -0600

    #13 fetch item

--- ninjalist/components/Navbar.js
+++ ninjalist/components/Navbar.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Navbar = () => {
   return ( 
     <nav>
-      <div class="logo">
+      <div className="logo">
         <Image src="/logo.png" width={128} height={77} />
       </div>
       <Link href="/"><a>Home</a></Link>
