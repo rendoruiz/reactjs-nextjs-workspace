5a348be0add397054ae6e5bb0c6aeec350120180
rendoruiz <rendo.ruiz@outlook.com>
2021-08-18 13:48:45 -0600

    #2 - pages & routing

--- ninjalist/pages/about.js
+++ ninjalist/pages/about.js
@@ -1,13 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
+const About = () => {
+  
+  return ( 
     <div>
-      <h1>Homepage</h1>
+      <h1>About</h1> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam nisi, amet quo quidem tenetur obcaecati placeat voluptate exercitationem vel ullam atque. Repellendus animi dolorum laboriosam rem similique facilis praesentium!</p>
     </div>
-  )
+  );
 }
+ 
+export default About;
