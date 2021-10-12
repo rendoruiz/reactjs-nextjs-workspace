11eb159679e50e45abb0193f0f3f0da0c9452a7e
rendoruiz <rendo.ruiz@outlook.com>
2021-10-12 13:03:29 -0600

    style changes

--- todo-app/components/TodoInput.js
+++ todo-app/components/TodoInput.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const TodoInput = ({ handleAdd }) => {
-  const [content, setContent] = useState(null);
+  const [content, setContent] = useState('');
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const contentInput = useRef(null);
 
   const handleInputChange = (e) => {
     setContent(e.target.value);
@@ -9,27 +11,56 @@ const TodoInput = ({ handleAdd }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleAdd(content)
+    if (isContentValid()) {
+      handleAdd(content.trim());
+    } else {
+      contentInput.current.focus();
+    }
+  }
+
+  const isContentValid = () => {
+    return content.trim().length > 0 ? true : false;
   }
 
   return ( 
     <form 
-      className="grid grid-cols-[1fr,auto] gap-1 p-1 rounded bg-gray-200 text-gray-700"
+      className="grid grid-cols-[auto,1fr] rounded p-1 bg-black/50 text-white backdrop-blur"
       onSubmit={handleSubmit}
     >
+      {/* Add new todo */}
+      <button 
+        title={isInputFocused && !isContentValid() ? "Cannot add empty todo" : "Add new todo item"}
+        className="w-10 h-full rounded p-2 transition-opacity disabled:opacity-20 disabled:cursor-not-allowed"
+        disabled={isInputFocused && !isContentValid()}
+      >
+        <svg 
+          class="w-6 h-6" 
+          fill="none" 
+          stroke="currentColor" 
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            stroke-width="2" 
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
+
+      {/* New todo content */}
       <input 
         type="text" 
         name="todo" 
-        title="New todo item content."
-        className="px-3 py-2 rounded"
+        title={!isInputFocused ? "Add new todo item" : "New todo item content."}
+        placeholder="Add new item"
+        className="pr-3 pl-1 py-2 rounded bg-transparent text-white placeholder-gray-100 cursor-pointer focus:cursor-text focus:placeholder-opacity-0"
+        ref={contentInput}
         onChange={handleInputChange}
+        onFocus={() => setIsInputFocused(true)}
+        onBlur={() => setIsInputFocused(false)}
       />
-      <button 
-        title="Add new todo item."
-        className="p-2 rounded bg-white transition-colors hover:bg-blue-400/50"
-      >
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
-      </button>
     </form>
   );
 }
--- todo-app/components/TodoItem.js
+++ todo-app/components/TodoItem.js
@@ -49,7 +49,20 @@ const TodoItem = ({ item, handleToggleComplete, handleDelete, handleFocus }) => 
         onClick={() => handleDelete(item.id)}
         className="grid place-items-center p-2 w-10 h-full rounded text-gray-500 transition-colors hover:bg-gray-200 hover:text-red-400"
       >
-        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+        <svg 
+          className="w-5 h-5" 
+          fill="none" 
+          stroke="currentColor" 
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            strokeLinecap="round" 
+            strokeLinejoin="round" 
+            strokeWidth="2" 
+            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+          />
+        </svg>
       </button>
     </li>
   );
